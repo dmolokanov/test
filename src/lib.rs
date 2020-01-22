5cc5fcf81cce84c9c9f1df4fe78f5404a7d4f7bc
dmolokanov <dmolokanov@users.noreply.github.com>
2020-01-22 10:58:37 -0800

    add mor rust code

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@
 mod tests {
     #[test]
     fn it_works() {
+        let a = super::A::new(3);
+        let mut b = a;
+
         assert_eq!(2 + 2, 4);
     }
 
@@ -9,4 +12,27 @@ mod tests {
     fn it_also_works() {
         assert_eq!(2 + 2, 4);
     }
+}
+
+// some comments
+pub struct A {
+    pub b: i32,
+}
+
+impl A {
+    pub fn new(b: i32) -> Self {
+        let a: bool = true;
+        println!("a log message");
+        Self { b }
+    }
+}
+
+pub enum B {
+    V1,
+    V2(String),
+    V3(bool),
+}
+
+pub struct Gen<'a, T> {
+    t: &'a T,
 }
